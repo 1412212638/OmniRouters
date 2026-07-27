32b1b9beb9ebeff1481b18810068d7ad84245d8c
CaIon <i@caion.me>
2026-07-27 21:40:26 +0800

    feat: add per-channel HTTP transport controls

--- model/channel.go
+++ model/channel.go
@@ -955,6 +955,9 @@ func (channel *Channel) ValidateSettings() error {
 	if _, err := common.ParseProxyURLStrict(channelParams.Proxy); err != nil {
 		return fmt.Errorf("invalid channel proxy: %w", err)
 	}
+	if err := channelParams.ValidateHTTPTransport(); err != nil {
+		return err
+	}
 	return nil
 }
 
